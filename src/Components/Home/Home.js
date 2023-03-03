4e45f1b7701c47163c4e2a0d5e23ec31f9b6ec7c
Akhil-5292 <aps.ca9@gmail.com>
2023-03-04 00:47:25 +0530

    updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,25 +3,23 @@ import {  Fab } from '@mui/material';
 import style from './Home.module.css'
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
-import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
-import ReorderIcon from '@mui/icons-material/Reorder';
-import SegmentIcon from '@mui/icons-material/Segment';
 import DownloadIcon from '@mui/icons-material/Download';
 import FormatBoldIcon from "@mui/icons-material/FormatBold";
 import FormatItalicIcon from "@mui/icons-material/FormatItalic";
 import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
-// import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
-// import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
-// import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
-// import CodeIcon from "@mui/icons-material/Code";
-// import FormatColorFillIcon from "@mui/icons-material/FormatColorFill";
-// import FormatColorTextIcon from "@mui/icons-material/FormatColorText";
-// import FormatIndentDecreaseIcon from "@mui/icons-material/FormatIndentDecrease";
-// import FormatIndentIncreaseIcon from "@mui/icons-material/FormatIndentIncrease";
-// import FormatLineSpacingIcon from "@mui/icons-material/FormatLineSpacing";
-// import FormatSizeIcon from "@mui/icons-material/FormatSize";
-// import ImageIcon from "@mui/icons-material/Image";
-import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded";
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
+import CodeIcon from "@mui/icons-material/Code";
+import FormatColorFillIcon from "@mui/icons-material/FormatColorFill";
+import FormatColorTextIcon from "@mui/icons-material/FormatColorText";
+import FormatIndentDecreaseIcon from "@mui/icons-material/FormatIndentDecrease";
+import FormatLineSpacingIcon from "@mui/icons-material/FormatLineSpacing";
+import FormatSizeIcon from "@mui/icons-material/FormatSize";
+import ImageIcon from "@mui/icons-material/Image";
+import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import ZoomOutIcon from '@mui/icons-material/ZoomOut';
+import UndoIcon from '@mui/icons-material/Undo';
+import RedoIcon from '@mui/icons-material/Redo';
+
 export default function Home(){
     
    
@@ -78,38 +76,30 @@ export default function Home(){
 
     return(
         <div className={style.wrapper}>
-        <div className={style.nav}> 
-        <button id="boldButton">B</button>
-        <button id="italicsButton">I</button>
-        <button id="underlineButton">U</button>
-        <button>"</button>    
-        <button>H1</button>    
-        <button>H2</button>    
-        <button><FormatListBulletedIcon/></button>    
-        <button><FormatListNumberedIcon/></button>
-        <button>Normal</button>
-        <button><UnfoldMoreIcon/></button>
-        <button><ReorderIcon/></button>
-        <button><SegmentIcon/></button>
-        {/* <div className={style.IconsContainer}>
-          <FormatBoldIcon id="boldButton" />
-          <FormatItalicIcon id="italicButton" />
-          <FormatUnderlinedIcon id="underlineButton" />
-          <FormatListBulletedIcon />
-          <FormatListNumberedIcon />
-          <FormatQuoteIcon />
-          <CodeIcon />
-          <FormatColorFillIcon />
-          <FormatColorTextIcon />
-          <FormatIndentDecreaseIcon />
-          <FormatIndentIncreaseIcon />
-          <FormatLineSpacingIcon />
-          <FormatSizeIcon />
-          <ImageIcon />
-        </div> */}
-        {/* <div> */}
+        <div className={style.IconsContainer}> 
+        <UndoIcon/>
+        <RedoIcon/>
+        <ZoomInIcon/>
+        <ZoomOutIcon/>
+        <FormatBoldIcon id="boldButton"/>
+        <FormatItalicIcon id="italicsButton"/>
+        <FormatUnderlinedIcon id="underlineButton"/>
+        <FormatQuoteIcon/>
+        <CodeIcon/>
+        <FormatColorFillIcon/>
+        <FormatListBulletedIcon/>    
+        <FormatListNumberedIcon/>
+        <FormatColorTextIcon/>
+        <FormatIndentDecreaseIcon/>
+        <FormatLineSpacingIcon/>
+        <FormatSizeIcon/>
+        <ImageIcon/>
+      
+        
+   
+        <div>
         <button id="New">New</button>
-        {/* </div> */}
+       </div>
                 
 
          </div>
